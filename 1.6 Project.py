aac0d5c05dce48b090587ea122f486314a88de17
Bennettii99 <116778780+Bennettii99@users.noreply.github.com>
2025-11-14 12:39:57 -0500

    Add files via upload

---
+++ 1.6 Project.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+# Display the application title
+print("Ishbennett's Excel Spreadsheet Automation Menu")
+
+# Display the menu options
+print("Choose a number from the following options")
+print("1. Input Data")
+print("2. View Current Data")
+print("3. Generate Report")
+
+# The next line retrieves the inputted option and stores into the variable called choice.
+choice = input()
+
+# Display the selected option with current date and time
+print(f"You selected {choice} at {datetime.now()}")
